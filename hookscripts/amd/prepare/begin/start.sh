47dbc5b4a98bd993675e7d0361f593ce527fe0fd
Gnarus-G <37311893+Gnarus-G@users.noreply.github.com>
2023-03-04 21:59:58 -0500

    Update start.sh

--- hookscripts/amd/prepare/begin/start.sh
+++ hookscripts/amd/prepare/begin/start.sh
@@ -5,28 +5,23 @@ set -x
 #load variables
 source /etc/libvirt/hooks/kvm.conf
 
-# Stop your display manager. If youre on kde it ll be sddm.service. Gnome users should use killall gdm-x-session instead
+# Stop your display manager.
 systemctl stop display-manager.service
-killall gdm-x-session
-pulse_pid=$(pgrep -u gnarus pulseaudio)
-pipewire_pid=$(pgrep -u gnarus pipewire-media)
-kill $pulse_pid
-kill $pipewire_pid
+
+# Stop audio
+pkill pulseaudio
 
 # Unbind VTconsoles
 echo 0 > /sys/class/vtconsole/vtcon0/bind
 
+# Unbind EFI Framebuffer
+# echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
 # Avoid a race condition by waiting a couple of seconds. This can be calibrated to be shorter or longer if required for your system
-sleep 2
+sleep 5
 
 # Unload all Radeon drivers
-
 modprobe -r amdgpu
-modprobe -r gpu_sched
-modprobe -r ttm
-modprobe -r drm_kms_helper
-modprobe -r i2c_algo_bit
-modprobe -r drm
 
 # Unbind the GPU from display driver
 virsh nodedev-detach $VIRSH_GPU_VIDEO
