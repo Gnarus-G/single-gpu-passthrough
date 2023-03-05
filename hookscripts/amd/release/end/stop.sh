6314e3f49785908046e574944431dff66e708932
Gnarus-G <37311893+Gnarus-G@users.noreply.github.com>
2023-03-04 22:00:35 -0500

    Update stop.sh

--- hookscripts/amd/release/end/stop.sh
+++ hookscripts/amd/release/end/stop.sh
@@ -4,10 +4,10 @@ set -x
 
 #load variables
 source /etc/libvirt/hooks/kvm.conf
-  
+
 # Unload all the vfio modules
-modprobe -r vfio_pci
 modprobe -r vfio_iommu_type1
+modprobe -r vfio_pci
 modprobe -r vfio
 
 # Rebind VT consoles
@@ -18,14 +18,10 @@ virsh nodedev-reattach $VIRSH_GPU_VIDEO
 virsh nodedev-reattach $VIRSH_GPU_AUDIO
 
 # Load all Radeon drivers
+modprobe amdgpu
 
-modprobe  amdgpu
-modprobe  gpu_sched
-modprobe  ttm
-modprobe  drm_kms_helper
-modprobe  i2c_algo_bit
-modprobe  drm
-
+# Bind EFI Framebuffer
+# echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/bind
 
 # Restart Display Manager
 systemctl start display-manager.service
